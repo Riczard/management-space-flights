7da8f387db0c15fcb6b77ee1d8e5fbc2b9898711
Riczard <riczardkiller@gmail.com>
2019-06-27 16:06:56 +0200

    add skeleton project with pom

---
+++ src/main/java/kuklinski/spaceflights/SpaceflightsApplication.java
@@ -0,0 +1,13 @@
+package kuklinski.spaceflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceflightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceflightsApplication.class, args);
+    }
+
+}
