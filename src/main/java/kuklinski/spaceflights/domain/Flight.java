e5b359de59c362a1d8f5c5b41c38a27a3f635001
Riczard <riczardkiller@gmail.com>
2019-06-28 17:24:17 +0200

    Change date format, field names, add ManyToMany relation

--- src/main/java/kuklinski/spaceflights/domain/Flight.java
+++ src/main/java/kuklinski/spaceflights/domain/Flight.java
@@ -1,14 +1,16 @@
 package kuklinski.spaceflights.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Data
 @Entity
+@Table(name = "flights")
 public class Flight {
 
     @Id
@@ -16,25 +18,34 @@ public class Flight {
     private long id;
 
     @Column
-    private DateTimeFormatter departureDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp departureDate;
 
     @Column
-    private DateTimeFormatter arrivalDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp arrivalDate;
 
     @Column
     private int numberOfSeats;
 
-    @OneToMany
-    private List<Tourist> listOfTourist;
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE,
+            })
+    @JoinTable(name = "flight_tourist",
+            joinColumns = {@JoinColumn(name = "flight_id")},
+            inverseJoinColumns = {@JoinColumn(name = "tourist_id")})
+    private List<Tourist> tourists;
 
     @Column
     private double price;
 
     public void addTourist(Tourist tourist) {
-        listOfTourist.add(tourist);
+        tourists.add(tourist);
     }
 
     public void removeTourist(Tourist tourist) {
-        listOfTourist.remove(tourist);
+        tourists.remove(tourist);
     }
 }
